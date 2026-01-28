e57d696ad7c7ccc5275084344376b74b2e6b36be
Manoj Aelishala <158211255+Manoj-alishala@users.noreply.github.com>
2026-01-28 09:56:42 +0530

    Update tuesday_lab.py

--- Week 1/tuesday_lab.py
+++ Week 1/tuesday_lab.py
@@ -36,3 +36,79 @@ for _ in range(t):
 
 
 # Output : 240.000000
+
+
+
+
+# Problem 2 
+
+"""
+Sample Input:
+1
+7
+4 1 6 2 5 3 2
+
+Expected Output:
+1 2 2 3 4 5 6
+"""
+
+def merge(left_half, right_half):
+    sorted_arr = []
+    i = 0
+    j = 0
+    
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] <= right_half[j]:
+            sorted_arr.append(left_half[i])
+            i += 1
+        else:
+            sorted_arr.append(right_half[j])
+            j += 1
+    
+    
+    sorted_arr.extend(left_half[i:])
+    sorted_arr.extend(right_half[j:])
+    
+    return sorted_arr
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    # Divide
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    
+    left_sorted = merge_sort(left_half)
+    right_sorted = merge_sort(right_half)
+    
+    return merge(left_sorted, right_sorted)
+
+def solve():
+    try:
+
+        input_line = input().strip()
+        if not input_line:
+            return
+        t = int(input_line)
+        
+        for _ in range(t):
+            
+            try:
+                n = int(input().strip())
+            except EOFError:
+                break
+                
+            arr = list(map(int, input().strip().split()))
+            
+            result = merge_sort(arr)
+            
+            print(*result)
+            
+    except EOFError:
+        pass
+
+if __name__ == "__main__":
+    solve()
